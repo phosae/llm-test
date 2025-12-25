f71437f73d4291ddc634794e468f3d4aeb1d901d
phosae <zengxu_121@126.com>
2025-12-25 20:35:43 +0800

    proxy: support dump

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -8,17 +8,43 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
+	"sync/atomic"
+	"time"
 )
+
+var requestCounter uint64
+
+type dumpJob struct {
+	filename string
+	data     []byte
+}
+
+var dumpChan chan dumpJob
+
+func dumpWorker() {
+	for job := range dumpChan {
+		os.WriteFile(job.filename, job.data, 0644)
+	}
+}
 
 func main() {
 	targetUrl := flag.String("origin", "", "Target URL")
 	port := flag.Int("p", 8000, "Port")
+	dump := flag.Bool("dump", false, "Dump each request/response to file in current dir")
 	flag.StringVar(targetUrl, "o", "https://api.ppinfra.com", "Target URL (shorthand)")
 	flag.Parse()
-	// 固定转发目标
+	if *dump {
+		dumpChan = make(chan dumpJob, 100)
+		go dumpWorker()
+	}
+
 	target, _ := url.Parse(*targetUrl)
 
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		reqID := atomic.AddUint64(&requestCounter, 1)
+		timestamp := time.Now().Format("20060102_150405")
 
 		// ---- 打印请求 ----
 		reqDump, _ := httputil.DumpRequest(r, true)
@@ -48,6 +74,12 @@ func main() {
 		fmt.Println("=== Upstream Response ===")
 		respDump, _ := httputil.DumpResponse(resp, true)
 		fmt.Println(string(respDump))
+
+		if *dump {
+			data := append(reqDump, []byte("\n\n---\n\n")...)
+			data = append(data, respDump...)
+			dumpChan <- dumpJob{fmt.Sprintf("%s_%d_%d.txt", timestamp, reqID, resp.StatusCode), data}
+		}
 
 		// ---- 回传响应 ----
 		for k, v := range resp.Header {
